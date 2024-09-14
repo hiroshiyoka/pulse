a6dade45349b0a1d7fb09d3bbe7d2d8d114f5421
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-09-15 00:55:15 +0700

    feat(docs): add header component to docs page

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,8 +1,14 @@
 import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/header";
 
 const Document = () => {
   return (
     <div>
+      <Header>
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className="document-title">Document title example</p>
+        </div>
+      </Header>
       <Editor />
     </div>
   );
