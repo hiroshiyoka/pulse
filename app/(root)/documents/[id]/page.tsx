57ba8ac3044e400c90455c2c90dd6ad4619595d1
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-09-22 00:15:41 +0700

    feat(components): add collaborative room provider component

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,24 +1,5 @@
-import Header from "@/components/header";
-import { Editor } from "@/components/editor/Editor";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-
 const Document = () => {
-  return (
-    <div>
-      <Header>
-        <div className="flex w-fit items-center justify-center gap-2">
-          <p className="document-title">Document title example</p>
-        </div>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </Header>
-      <Editor />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Document;
