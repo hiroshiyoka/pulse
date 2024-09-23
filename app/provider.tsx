4ab5c1391aae7ba73401590637341e601c2d4da4
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-09-24 02:10:10 +0700

    feat(app): add update to app liveblocks provider

--- app/provider.tsx
+++ app/provider.tsx
@@ -7,10 +7,17 @@ import {
   LiveblocksProvider,
 } from "@liveblocks/react/suspense";
 import Loader from "@/components/loader";
+import { getClerkUsers } from "@/lib/actions/user.actions";
 
 const Provider = ({ children }: { children: ReactNode }) => {
   return (
-    <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
+    <LiveblocksProvider
+      authEndpoint="/api/liveblocks-auth"
+      resolveUsers={async ({ userIds }) => {
+        const users = await getClerkUsers({ userIds });
+        return users;
+      }}
+    >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
     </LiveblocksProvider>
   );
