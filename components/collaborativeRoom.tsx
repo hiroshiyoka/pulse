3bc3c035e9dc0fb76f576e67c301c8f7c4e11793
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-09-25 09:22:43 +0700

    feat(components): add update to collaborative room component

--- components/collaborativeRoom.tsx
+++ components/collaborativeRoom.tsx
@@ -6,22 +6,29 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Loader from "./loader";
 import Header from "./header";
 import { Editor } from "./editor/Editor";
+import ActiveCollaborators from "./activeCollaborators";
 
-const CollaborativeRoom = () => {
+const CollaborativeRoom = ({
+  roomId,
+  roomMetadata,
+}: CollaborativeRoomProps) => {
   return (
-    <RoomProvider id="my-room">
+    <RoomProvider id={roomId}>
       <ClientSideSuspense fallback={<Loader />}>
         <div className="collaborative-room">
           <Header>
             <div className="flex w-fit items-center justify-center gap-2">
               <p className="document-title">Document title example</p>
             </div>
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
+            <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
+              <ActiveCollaborators />
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
           </Header>
           <Editor />
         </div>
